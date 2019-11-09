abe6af68cce6079307c9bc7c767b46bbce286830
zhangp <zhangp@differsoft.com>
2019-11-09 22:31:30 +0800

    Vue基础布局Demo

---
+++ user/src/pages/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+<style></style>
+<script>
+    export default {
+        data() {
+            return {msg: 'hello vue!,this is Dashboard page'}
+        }
+    }
+</script>
--- user/src/router/index.js
+++ user/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+// import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+import Main from '@/pages/Main'
+import Dashboard from '@/pages/Dashboard'
+import User from '@/pages/User'
+
+let routes = [{
+  path: '/',
+  name: 'Main',
+  component: Main,
+  hidden: true,
+  children: [{
+    path: '/',
+    component: Dashboard,
+    name: '系统首页'
+  },
+  {
+    path: 'user',
+    component: User,
+    name: '用户管理'
+  }]
+}]
+
+const router = new Router({
+  routes: routes
 })
+
+export default router
