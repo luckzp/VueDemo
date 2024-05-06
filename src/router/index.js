408d1e1a50cd7a5d1086b3e998c0654e0f50685c
zhangpeng <zhangpeng@wedobest.com.cn>
2024-05-06 18:10:11 +0800

    feat: 精简代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
-
-import Main from '@/views/Main'
-import Login from '@/views/login'
-import ToDoList from '@/views/ToDoList'
-import Home from '@/views/home'
-import GAME2048 from '@/views/2048'
-import UserCenter from '@/views/UserCenter'
 import User from '@/views/User'
-import Order from '@/views/Order'
-import About from '@/views/About'
-import Banner from '@/components/header'
+import Main from '@/views/Main'
 
 
 let routes = [
-  {
+ {
     path: '/',
-    name: 'Home',
-    component: Home,
-  }, {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },{
-    path: '/banner',
-    name: 'Banner',
-    component: Banner,
-  },
-  {
-    path: '/2048',
-    name: 'GAME2048',
-    component: GAME2048,
-  },
-  {
-    path: '/todolist',
-    name: 'ToDoList',
-    component: ToDoList,
-  },
-  {
-    path: '/about',
-        component: About,
-        name: 'About'
-  },
-   {
-    path: '/user-center',
-    name: 'UserCenter',
-    component: UserCenter,
-    children: [
-      {
-        path: '/order',
-        component: Order,
-        name: 'Order'
-      },
-      {
-        path: '/about',
-        component: About,
-        name: 'About'
-      }]
-  }, {
-    path: '/main',
     name: 'Main',
     component: Main,
     children: [
